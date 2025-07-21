72e429758d189e9707c2b0c904181b9a5a3dd2ca
fondy <fondy752@gmail.com>
2025-07-21 16:21:16 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  base: process.env.NODE_ENV === 'production'
+    ? '/vite-savor/'  // 生产环境使用仓库名作为基础路径
+    : '/',                 // 开发环境使用根路径
+  plugins: [vue()]
 })
 
 
